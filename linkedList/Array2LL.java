f64fa712b4206321a52b366999409b413c38caa2
pachauriaditya <pachauriaditya18@email.com>
2026-08-09 21:37:42 +0530

    Updated Linked List code

--- linkedList/Array2LL.java
+++ linkedList/Array2LL.java
@@ -1,225 +1,255 @@
-public class Array2LL{
-    public static class Node{
-         int data;
-         Node next;
+public class Array2LL {
 
-         public Node( int data , Node next){
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data, Node next) {
             this.data = data;
             this.next = next;
-         }
+        }
 
-         public Node( int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
-         }
+        }
+    }
 
-         private static Node arr2ll( int []arr){
-            Node head = new Node(arr[0]);
-            Node mover = head;
+    private static Node arr2ll(int[] arr) {
+        Node head = new Node(arr[0]);
+        Node mover = head;
 
-            for( int i = 1 ; i < arr.length ; i++){
-                Node temp = new Node(arr[i]);
-                mover.next= temp;
-                mover = temp;
+        for (int i = 1; i < arr.length; i++) {
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+
+        return head;
+    }
+
+    private static int size(Node head) {
+        int count = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            temp = temp.next;
+            count++;
+        }
+
+        return count;
+    }
+
+    private static int check(Node head, int target) {
+        Node temp = head;
+
+        while (temp != null) {
+            if (temp.data == target) {
+                return 1;
             }
+
+            temp = temp.next;
+        }
+
+        return 0;
+    }
+
+    private static void print(Node head) {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+
+        System.out.println("null");
+    }
+
+    private static Node removeHead(Node head) {
+        if (head == null) {
             return head;
-         }
+        }
 
-         private static int size(Node head){
-            int count = 0 ; 
-            Node temp = head;
-            while( temp != null){
-                temp = temp.next;
-                count++;
-            }
-            return count;
-         }
+        Node temp = head;
+        head = head.next;
 
-         private static int check(Node head , int target ){
-            Node temp = head;
-            while( temp != null){
-                if( temp.data == target){
-                    return 1;
-                }
-                temp = temp.next;
-            }
-            return 0;
-         }
+        return head;
+    }
 
-         private static void print(Node head){
-            Node temp = head;
-            while( temp != null){
-                System.out.print(temp.data + "->");
-                temp = temp.next;
-            }
-            System.out.println("null");
-         }
+    private static Node removeTail(Node head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
 
-         private static Node removeHead(Node head){
-            if( head == null){
-                return head;
-            }
+        Node temp = head;
 
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+
+        return head;
+    }
+
+    private static Node removeK(Node head, int k) {
+        if (head == null) {
+            return head;
+        }
+
+        if (k == 1) {
             Node temp = head;
             head = head.next;
 
             return head;
-         }
+        }
 
-         private static Node removeTail(Node head){
-            if( head == null || head.next == null){
-                return null;
+        Node temp = head;
+        Node prev = null;
+        int count = 0;
+
+        while (temp != null) {
+            count++;
+
+            if (count == k) {
+                prev.next = prev.next.next;
+                break;
             }
 
-            Node temp = head;
-            while( temp.next.next != null){
-                temp = temp.next;
-            }
-            temp.next = null;
-            return head;
-         }
+            prev = temp;
+            temp = temp.next;
+        }
 
-         private static Node removeK(Node head , int k){
-            if( head == null ){
-                return head;
-            }
-            if( k == 1){
-                Node temp = head;
-                head = head.next;
-                return head;
-            }
-            
-            Node temp = head;
-            Node  prev = null;
-            int count = 0;
-
-            while( temp != null){
-                count++;
-                if( count == k){
-                    prev.next = prev.next.next;
-                    break;
-                }
-                prev = temp;
-                temp = temp.next;
-            }
-            return head;
-         }
-
-         private static Node removeElement( Node head , int element){
-            if( head == null){
-                return head;
-            }
-
-            if( head.data == element){
-                head = head.next;
-                return head;
-            }
-
-            Node temp = head;
-            Node prev = null;
-            int count = 0;
-
-            while( temp != null){
-                count++;
-
-                if(count == element){
-                    prev.next = prev.next.next;
-                    break;
-                }
-                prev = temp;
-                temp = temp.next;
-            }
-            return head;
-         }
-
-         private static Node addhead(Node head , int val){
-           Node temp = new Node(val,head);
-           return temp;
-         }
-
-         private static Node addTail(Node head , int val){
-            if( head == null){
-                return head;
-            }
-
-            Node temp = head;
-
-            while( temp.next != null){
-                temp = temp.next;
-            }
-            Node newNode = new Node(val);
-            temp.next = newNode;
-            return head;
-         }
-
-         private static Node insertPosition(Node head , int el, int k){
-            if( head == null){
-                if( k == 1){
-                    return new Node(el);
-                }else{
-                    return null;
-                }
-            }
-
-             if( k == 1){
-                return new Node(el, head);
-            }
-
-            int count = 0;
-            Node temp = head;
-
-            while( temp != null){
-                count++;
-                if( count == k-1){
-                    Node x = new Node(el , temp.next);
-                    temp.next = x;
-                    break;
-                }
-            }
-            return head;
-         }
-
-         private static Node insertbeforevalue(Node head , int el, int val){
-            if( head == null){
-                return null;
-            }
-
-            if( head.data == val){
-                return new Node(el,head);
-            }
-
-            Node temp = head;
-            while( temp.next != null){
-                if(temp.next.data == val){
-                    Node x = new Node(el, temp.next);
-                    temp.next = x;
-                    break;
-                }
-                temp = temp.next;
-            }
-            return head;
-         }
+        return head;
     }
-}
+
+    private static Node removeElement(Node head, int element) {
+        if (head == null) {
+            return head;
+        }
+
+        if (head.data == element) {
+            head = head.next;
+            return head;
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            if (temp.next.data == element) {
+                temp.next = temp.next.next;
+                break;
+            }
+
+            temp = temp.next;
+        }
+
+        return head;
+    }
+
+    private static Node addhead(Node head, int val) {
+        Node temp = new Node(val, head);
+
+        return temp;
+    }
+
+    private static Node addTail(Node head, int val) {
+        if (head == null) {
+            return new Node(val);
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        Node newNode = new Node(val);
+        temp.next = newNode;
+
+        return head;
+    }
+
+    private static Node insertPosition(Node head, int el, int k) {
+        if (head == null) {
+            if (k == 1) {
+                return new Node(el);
+            } else {
+                return null;
+            }
+        }
+
+        if (k == 1) {
+            return new Node(el, head);
+        }
+
+        int count = 0;
+        Node temp = head;
+
+        while (temp != null) {
+            count++;
+
+            if (count == k - 1) {
+                Node x = new Node(el, temp.next);
+                temp.next = x;
+                break;
+            }
+
+            temp = temp.next;
+        }
+
+        return head;
+    }
+
+    private static Node insertbeforevalue(Node head, int el, int val) {
+        if (head == null) {
+            return null;
+        }
+
+        if (head.data == val) {
+            return new Node(el, head);
+        }
+
+        Node temp = head;
+
+        while (temp.next != null) {
+            if (temp.next.data == val) {
+                Node x = new Node(el, temp.next);
+                temp.next = x;
+                break;
+            }
+
+            temp = temp.next;
+        }
+
+        return head;
+    }
 
     public static void main(String[] args) {
-        int arr[] = {18,17,1,15,21,33};
 
-        Node head = convertArr2LL(arr);
+        int arr[] = {18, 17, 1, 15, 21, 33};
+
+        Node head = arr2ll(arr);
         System.out.println(head.data);
 
         Node temp = head;
 
-        while( temp != null){
+        while (temp != null) {
             System.out.print(temp.data + "->");
             temp = temp.next;
         }
+
         System.out.println("null");
-        System.out.println("Length of linked list: " + length(head));
+
+        System.out.println("Length of linked list: " + size(head));
 
         int valueToCheck = 8;
-        int isPresent = checkifPresent(head, valueToCheck);
-        System.out.println("Is " + valueToCheck + " present in the linked list? " + (isPresent == 1 ? "Yes" : "No"));
+        int isPresent = check(head, valueToCheck);
+
+        System.out.println(
+            "Is " + valueToCheck + " present in the linked list? "
+            + (isPresent == 1 ? "Yes" : "No")
+        );
 
         head = removeHead(head);
         print(head);
@@ -227,22 +257,22 @@ public class Array2LL{
         head = removeTail(head);
         print(head);
 
-        head = removeK(head,4);
+        head = removeK(head, 4);
         print(head);
 
         head = removeElement(head, 18);
         print(head);
 
-        head = addHead(head, 100);
+        head = addhead(head, 100);
         print(head);
 
         head = addTail(head, 200);
         print(head);
 
-        head = insertPostion(head, 500, 2);
+        head = insertPosition(head, 500, 2);
         print(head);
 
-        head = insertBeforeValue(head, 1000, 15);
-        print(head);    
+        head = insertbeforevalue(head, 1000, 15);
+        print(head);
     }
 }
--- linkedList/doublyLL.java
+++ linkedList/doublyLL.java
@@ -1,128 +1,246 @@
-// Problem: Implementation of Doubly Linked List (Insertion & Deletion)
-// Approach: Maintain prev and next pointers, perform operations (add/remove) at beginning, end, and middle using pointer manipulation
-// Time Complexity: O(1) for addFirst/addLast/removeFirst/removeLast, O(n) for addMiddle/removeMiddle
-// Space Complexity: O(1)
-
-public class doublyLL {
-    public class Node {
+public class doublyLL{
+    public static class Node{
         int data;
         Node next;
         Node prev;
 
-        public Node(int data) {
+        public Node(int data , Node next , Node prev){
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        public Node(int data){
             this.data = data;
             this.next = null;
             this.prev = null;
         }
     }
 
-    public static Node head;
-    public static Node tail;
-    public static int size;
+    private static  Node arr2DLL(int arr[]){
+        Node head = new Node(arr[0]);  // Node head = new Node(arr[0], null , null) ----> we didn't use this cuz the value of next and prev are already null in the second constructor 
+                                       // and we can also use it , referencing the first constructor
+        Node prev = head;
 
-    // add first
-    public void addFirst(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = tail = newNode;
-            return;
+        for( int i = 1 ; i < arr.length ; i++){
+            Node temp = new Node(arr[i], null , prev);
+            prev.next = temp;
+            prev = temp;
         }
-        newNode.next = head;
-        head.prev = newNode;
-        head = newNode;
-        size++;
+        return head;
     }
 
-    // add last
-    public void addLast(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = tail = newNode;
-            return;
-        }
-        tail.next = newNode;
-        newNode.prev = tail;
-        tail = newNode;
-        size++;
-    }
-
-    // add middle
-    public void addMiddle(int data) {
-        Node newNode = new Node(data);
-        if (head == null) {
-            head = tail = newNode;
-            return;
-        }
-        Node slow = head;
-        Node fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        newNode.next = slow.next;
-        slow.next.prev = newNode;
-        slow.next = newNode;
-        newNode.prev = slow;
-        size++;
-    }
-
-    // remove first
-    public void removeFirst() {
-        if (head == null) {
-            return;
-        }
-        head = head.next;
-        head.prev = null;
-        size--;
-    }
-
-    // remove last
-    public void removeLast() {
-        if (head == null) {
-            return;
-        }
-        tail = tail.prev;
-        tail.next = null;
-        size--;
-    }
-
-    // remove middle
-    public void removeMiddle() {
-        if (head == null) {
-            return;
-        }
-        Node slow = head;
-        Node fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        slow.prev.next = slow.next;
-        slow.next.prev = slow.prev;
-        size--;
-    }
-
-    // print
-    public void print() {
-        Node temp = head;
-        while (temp != null) {
-            System.out.print(temp.data + " <-> ");
-            temp = temp.next;
+    private static void print(Node head){
+        while (head != null) {
+            System.out.print(head.data + "<->");
+            head = head.next;
         }
         System.out.println("null");
     }
+    
+    private static Node removeHead(Node head){
+        if( head == null || head.next == null){
+            return null;
+        }
+        Node prev = head;
+        head = head.next;
+        head.prev = null;
+        prev.next = null;
+        return head;
+    }
 
+    private static Node removetail( Node head){
+        if( head == null || head.next == null){
+            return null;
+        }
+
+        Node tail = head;
+        while( tail.next != null ){
+            tail = tail.next;
+        }
+        Node newTail = tail.prev;
+        newTail.next = null;
+        tail.prev = null;
+        
+        return head;
+    }
+
+    private static Node removeatK(Node head , int k){
+
+        if( head == null){
+            return null;
+        }
+
+        Node temp = head;
+        int count = 0;
+
+        while( temp != null){
+            count++;
+            if( count == k){
+                break;
+            }
+            temp = temp.next;
+        }
+        Node back = temp.prev;
+        Node front = temp.next;
+
+        if( back == null && front == null){
+          return null;
+        }
+        else if( back == null){
+            return removeHead(head);
+        }
+        else if( front == null){
+            return removetail(head);
+        }
+        back.next = front;
+        front.prev = back;
+        temp.next = null;
+        temp.prev = null;
+
+        return head;
+    }
+
+    private static void deleteNode(Node temp) {
+
+    Node back = temp.next;
+    Node front = temp.prev;
+
+    if (front == null) {
+        back.prev = null;
+        temp.next = null;
+        return;
+    }
+
+    if (back == null) {
+        front.next = null;
+        temp.prev = null;
+        return;
+    }
+
+    back.prev = front;
+    front.next = back;
+
+    temp.next = null;
+    temp.prev = null;
+}
+   
+
+    private static Node addBeforeHead(Node head , int val){
+        Node newHead = new Node(val,head , null);
+        head.prev = newHead;
+        return newHead;
+    }
+
+   private static Node addAfterHead(Node head , int val){
+        Node newNode = new Node(val);
+        
+        newNode.next = head.next;
+        newNode.prev = head;
+        
+        if(head.next != null){
+            head.next.prev = newNode;
+        }
+        head.next = newNode;
+
+        return head;
+    }
+
+    private static Node addBeforeTail(Node head, int val){
+       if(head.next == null){
+        return addBeforeHead(head, val);
+       }
+        Node tail = head;
+        while( tail.next != null){
+            tail = tail.next;
+        }
+        Node back = tail.prev;
+
+        Node newNode = new Node(val , tail,back);
+        back.next = newNode;
+        tail.prev = newNode;
+        return head;
+    }
+
+    private static Node addAfterTail(Node head , int val){
+        if( head == null){
+              return new Node(val);
+        }
+
+        Node tail = head;
+
+        while( tail.next != null){
+            tail = tail.next;
+        }
+
+        Node newNode = new Node(val);
+        newNode.prev = tail;
+        newNode.next = null;
+
+        tail.next = newNode;
+
+        return head;
+    }
+
+    private static Node addBeforeK(Node head , int k , int val){
+        if(k == 1){
+            return addBeforeHead(head, val);
+        }
+        Node temp = head;
+        int count = 0;
+
+        while( temp != null){
+            count++;
+            if( count == k)
+                break;
+
+            temp = temp.next;
+        }
+            Node back = temp.prev;
+            Node newNode = new Node(val,temp,back);
+            back.next = newNode;
+            temp.prev = newNode;
+
+            return head;
+        
+    }
+
+    private static void addbeforeNode(Node node , int val){
+        Node back = node.prev;
+        Node newNode = new Node(val, node , back);
+        back.next = newNode;
+        node.prev = newNode;
+    }
     public static void main(String[] args) {
-        doublyLL dll = new doublyLL();
-        dll.addFirst(1);
-        dll.addFirst(2);
-        dll.addLast(3);
-        dll.addLast(4);
-        dll.addMiddle(5);
-        dll.print();
-        dll.removeFirst();
-        dll.removeLast();
-        dll.removeMiddle();
-        dll.print();
+        int[] arr = {1,3,2,4,5,6,7};
+        Node head = arr2DLL(arr);
+        print(head);
+        head = removeHead(head);
+        print(head);
+
+        head = removetail(head);
+        print(head);
+
+        head = removeatK(head,2);
+        print(head);
+
+        Node temp = head.next;
+        deleteNode(temp);
+        print(head);
+        head = addBeforeHead(head, 10);
+        print(head);
+        head = addAfterHead(head, 21);
+        print(head);
+
+        head = addBeforeTail(head, 12);
+        print(head);
+
+        head = addAfterTail(head, 8);
+        print(head);
+        head = addBeforeK(head, 2, 22);
+        print(head);
+
+        addbeforeNode(head.next, 100);
+        print(head);
     }
 }
